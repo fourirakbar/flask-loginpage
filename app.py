f92fe5dc7535f25b4f735451a11ccfcfe341bb55
fourirakbar <fourir.akbar@gmail.com>
2018-03-27 14:28:16 +0700

    add css + route logout

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ def do_admin_login():
     else:
         flash('wrong password!')
     return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
  
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
