b5e5f143701eec68f5f0c368a53ba3bf5e7d0815
fourirakbar <fourir.akbar@gmail.com>
2018-03-29 16:43:52 +0700

    add requests post

--- app.py
+++ app.py
@@ -4,32 +4,57 @@ from flask import request
 from flask import jsonify
 import json
 import os
+import requests
 from sqlalchemy.orm import sessionmaker
 from tabledef import *
+from flask_socketio import SocketIO
 engine = create_engine('sqlite:///loginits.db', echo=True)
  
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
  
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        ip_client = jsonify({'ip': request.remote_addr}), 200
+        # ip_client = jsonify({'ip': request.remote_addr}), 200
+        # dictToSend = {'question':json=dictToSend'what is the answer?'}
+        
+        # print 'response from server: ', res.text
+        # dictFromServer = res.json()
         result = request.form
+        # ip_client = request.remote_addr
+        # ip_client = {'ip': 'request.remote_addr'}
+        # res = requests.post('http://10.151.36.38:5000/test/endpoint', data=ip_client)
+        # return res.text
         return render_template('welcome.html', ip_client = request.remote_addr, result = result)
  
 @app.route('/login', methods=['POST'])
 def do_admin_login():
+    print "fungsi 1"
     POST_USERNAME = str(request.form['username'])
     POST_PASSWORD = str(request.form['password'])
  
     Session = sessionmaker(bind=engine)
     s = Session()
+    print "====="
     query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
     result = query.first()
+    print "sebelum if"
     if result:
         session['logged_in'] = True
+        ip_client = POST_USERNAME + "|" + request.remote_addr
+        # ip_client = jsonify({'ip': request.remote_addr}), 200
+        print "masuk"
+        res = requests.post('http://10.151.36.38:5000/tests/endpoint', headers={'content-type': 'application/json'}, json=ip_client)
+
+        # dictToSend = {'question':'what is the answer?'}
+        # res = requests.post('10.151.36.38:5000/test/endpoint', json=dictToSend)
+        # print 'response from server: ', res.text
+        # ductFromServer = res.json()
+
     else:
         flash('wrong password!')
     return home()
@@ -42,6 +67,25 @@ def logout():
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
     ip_client = jsonify({'ip': request.remote_addr}), 200
+
+@app.route("/query-example")
+def query_exampe():
+    username = request.args.get('username')
+    your_ip = request.remote_addr
+    return '''<h1>Your name is: {}</h1>
+            <h1>Your IP is: {}</h1>'''.format(username, your_ip)
+
+@app.route("/form-example", methods=['GET', 'POST'])
+def form_example():
+    return '''<form method="POST">
+                  Username: <input type="text" name="username"><br>
+                  Password: <input type="text" name="password"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+
+@app.route("/json-example", methods=['POST'])
+def json_exampe():
+    return 'Todo...'
  
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
